1696c47d6be06512ea830fc231040789f507f121
Makdoom Shaikh <makshaikh99@gmail.com>
2024-09-08 13:22:48 +0530

    Add google/github providers

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -3,14 +3,32 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  const onClickHandler = (providerName: "google" | "github") => {
+    signIn(providerName, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button size="lg" className="w-full" variant="secondary">
+      <Button
+        size="lg"
+        className="w-full"
+        variant="secondary"
+        onClick={() => onClickHandler("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button size="lg" className="w-full" variant="secondary">
+      <Button
+        size="lg"
+        className="w-full"
+        variant="secondary"
+        onClick={() => onClickHandler("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
